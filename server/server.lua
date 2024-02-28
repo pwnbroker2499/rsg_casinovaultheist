447785fb12153e983e8ba9135a87aeb709bec0e6
pwnbroker2499 <dlambert2499@gmail.com>
2024-02-28 15:22:49 -0700

    Completed first pass

--- server/server.lua
+++ server/server.lua
@@ -1,53 +1,53 @@
-local sharedItems = exports['qbr-core']:GetItems()
+local RSGCore = exports['rsg-core']:GetCoreObject()
 
 -- give reward
 RegisterServerEvent('rsg_casinovaultheist:server:reward')
 AddEventHandler('rsg_casinovaultheist:server:reward', function()
     local src = source
-    local Player = exports['qbr-core']:GetPlayer(src)
+    local Player = RSGCore.Functions.GetPlayer(src)
 	local randomNumber = math.random(1,3)
 	if randomNumber == 1 then
 		Player.Functions.AddItem('goldbar', math.random(1,3))
-		TriggerClientEvent('inventory:client:ItemBox', src, sharedItems['goldbar'], "add")
+		TriggerClientEvent('inventory:client:ItemBox', src, RSGCore.Shared.Items['goldbar'], "add")
 	elseif randomNumber == 2 then
 		Player.Functions.AddItem('goldbar', math.random(2,4))
-		TriggerClientEvent('inventory:client:ItemBox', src, sharedItems['goldbar'], "add")
+		TriggerClientEvent('inventory:client:ItemBox', src, RSGCore.Shared.Items['goldbar'], "add")
 	elseif randomNumber == 3 then
 		Player.Functions.AddItem('goldbar', math.random(4,8))
-		TriggerClientEvent('inventory:client:ItemBox', src, sharedItems['goldbar'], "add")
+		TriggerClientEvent('inventory:client:ItemBox', src, RSGCore.Shared.Items['goldbar'], "add")
 	end
 end)
 
 RegisterServerEvent('rsg_casinovaultheist:server:reward1')
 AddEventHandler('rsg_casinovaultheist:server:reward1', function()
     local src = source
-    local Player = exports['qbr-core']:GetPlayer(src)
+    local Player = RSGCore.Functions.GetPlayer(src)
 	local randomNumber = math.random(1,3)
 	if randomNumber == 1 then
 		Player.Functions.AddItem('bloodmoney', math.random(1,3))
-		TriggerClientEvent('inventory:client:ItemBox', src, sharedItems['bloodmoney'], "add")
+		TriggerClientEvent('inventory:client:ItemBox', src, RSGCore.Shared.Items['bloodmoney'], "add")
 	elseif randomNumber == 2 then
 		Player.Functions.AddItem('bloodmoney', math.random(2,4))
-		TriggerClientEvent('inventory:client:ItemBox', src, sharedItems['bloodmoney'], "add")
+		TriggerClientEvent('inventory:client:ItemBox', src, RSGCore.Shared.Items['bloodmoney'], "add")
 	elseif randomNumber == 3 then
 		Player.Functions.AddItem('bloodmoney', math.random(4,8))
-		TriggerClientEvent('inventory:client:ItemBox', src, sharedItems['bloodmoney'], "add")
+		TriggerClientEvent('inventory:client:ItemBox', src, RSGCore.Shared.Items['bloodmoney'], "add")
 	end
 end)
 
 RegisterServerEvent('rsg_casinovaultheist:server:reward2')
 AddEventHandler('rsg_casinovaultheist:server:reward2', function()
     local src = source
-    local Player = exports['qbr-core']:GetPlayer(src)
+    local Player = RSGCore.Functions.GetPlayer(src)
 	local randomNumber = math.random(1,3)
 	if randomNumber == 1 then
 		Player.Functions.AddItem('smallnugget', math.random(1,3))
-		TriggerClientEvent('inventory:client:ItemBox', src, sharedItems['smallnugget'], "add")
+		TriggerClientEvent('inventory:client:ItemBox', src, RSGCore.Shared.Items['smallnugget'], "add")
 	elseif randomNumber == 2 then
 		Player.Functions.AddItem('mediumnugget', math.random(2,4))
-		TriggerClientEvent('inventory:client:ItemBox', src, sharedItems['mediumnugget'], "add")
+		TriggerClientEvent('inventory:client:ItemBox', src, RSGCore.Shared.Items['mediumnugget'], "add")
 	elseif randomNumber == 3 then
 		Player.Functions.AddItem('largenugget', math.random(4,8))
-		TriggerClientEvent('inventory:client:ItemBox', src, sharedItems['largenugget'], "add")
+		TriggerClientEvent('inventory:client:ItemBox', src, RSGCore.Shared.Items['largenugget'], "add")
 	end
 end)
